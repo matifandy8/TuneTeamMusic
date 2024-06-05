189532c99928b28dff41fd9717227cec8ff22b79
matias <matifandy@gmail.com>
2024-06-05 20:32:33 -0300

    fix: fix some desing styles

--- src/app/room/[id]/page.tsx
+++ src/app/room/[id]/page.tsx
@@ -1,6 +1,11 @@
 import styles from "./page.module.css";
 
 export default function Page({ params }: { params: { id: string } }) {
+  // TODO: check if room exists on db and return a page with the info of the room
+  // get info from db and pass it to the page component
+
+  const roomId = params.id;
+
   return (
     <div className={styles.container}>
       <div className={styles.content}>
